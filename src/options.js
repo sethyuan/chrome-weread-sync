b7c31cfb9bb9db03802c4ea496420e50d605cc90
Seth <sethyuan@gmail.com>
2023-02-15 22:09:20 +0800

    feat: options page and basic structure

---
+++ src/options.js
@@ -0,0 +1,26 @@
+async function main() {
+  const config = await chrome.storage.local.get({
+    apiUrl: "http://127.0.0.1:12315",
+    token: "",
+  })
+  document.getElementById("apiUrl").value = config.apiUrl
+  document.getElementById("token").value = config.token
+
+  const saveBtn = document.getElementById("saveBtn")
+  saveBtn.addEventListener("click", save)
+}
+
+async function save() {
+  await chrome.storage.local.set({
+    apiUrl: document.getElementById("apiUrl").value,
+    token: document.getElementById("token").value,
+  })
+
+  const statusEl = document.getElementById("status")
+  statusEl.textContent = "保存成功"
+  setTimeout(() => {
+    statusEl.textContent = ""
+  }, 750)
+}
+
+document.addEventListener("DOMContentLoaded", main)
--- src/popup.js
+++ src/popup.js
@@ -6,16 +6,88 @@ async function main(...args) {
     lastFocusedWindow: true,
   })
   if (SITE_URLS.some((url) => tab.url.startsWith(url))) {
-    const ret = await chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: foo,
-    })
-    console.log(ret)
+    await sync(tab)
+  } else {
+    await openWeReadTab()
   }
 }
 
-async function foo() {
-  return document.title
+async function sync(tab) {
+  const msgEl = document.getElementById("msg")
+
+  msgEl.textContent = "获取书籍数据……"
+  const [{ result: booksRes }] = await chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: getBooks,
+  })
+  if (booksRes.code !== 200) {
+    return errorMsg(booksRes.code)
+  }
+
+  msgEl.textContent = "向Logseq导入书籍……"
+  const syncBooksRes = await syncBooks(booksRes.data)
+  if (syncBooksRes.code !== 200) {
+    return errorMsg(syncBooksRes.code)
+  }
+
+  if (syncBooksRes.data.toSync.length > 0) {
+    msgEl.textContent = "获取笔记数据……"
+    const [{ result: detailsRes }] = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: getDetailsAndNotes,
+      args: [syncBooksRes.data.toSync],
+    })
+    if (detailsRes.code !== 200) {
+      return errorMsg(detailsRes.code)
+    }
+
+    msgEl.textContent = "向Logseq导入笔记……"
+    const syncDetailsRes = await syncDetails(detailsRes.data)
+    if (syncDetailsRes.code !== 200) {
+      return errorMsg(syncDetailsRes.code)
+    }
+  }
+
+  msgEl.textContent = "完成"
 }
 
-main()
+async function getBooks() {
+  // TODO
+}
+
+async function getDetailsAndNotes(bookIds) {
+  // TODO
+}
+
+async function syncBooks(books) {
+  // TODO
+  const res = await fetch("")
+  if (!res.ok) return { code: res.code }
+  const json = await res.json()
+  return { code: 200, data: json }
+}
+
+async function syncDetails(details) {
+  // TODO
+  const res = await fetch("")
+  if (!res.ok) return { code: res.code }
+  const json = await res.json()
+  return { code: 200, data: json }
+}
+
+function errorMsg(msgEl, code) {
+  switch (code) {
+    case 401:
+      msgEl.textContent = "请先登录微信读书"
+      break
+    default:
+      msgEl.textContent = "同步失败"
+      break
+  }
+}
+
+async function openWeReadTab() {
+  await chrome.tabs.create({ url: "https://weread.qq.com" })
+}
+
+document.addEventListener("DOMContentLoaded", main)
